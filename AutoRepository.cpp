aedf0e44404a0deb8fbf72ace1ff1686c0edea56
Radutiu-Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2020-04-09 13:24:00 +0300

    crudrepo

---
+++ AutoRepository.cpp
@@ -0,0 +1,37 @@
+#include "CrudRepo.h"
+#include "AutoRepository.h"
+
+using namespace std;
+
+AutoInRepo::AutoInRepo()
+{
+}
+
+AutoInRepo::~AutoInRepo()
+{
+}
+
+Auto AutoInRepo::findOne(int id)
+{
+
+}
+
+Auto AutoInRepo::findAll()
+{
+
+}
+
+Auto AutoInRepo::save()
+{
+
+}
+
+Auto AutoInRepo::del()
+{
+
+}
+
+Auto AutoInRepo::update()
+{
+
+}
---
+++ AutoRepository.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <vector>
+#include "CrudRepo.h"
+using namespace std;
+
+class AutoInRepo: public ​CrudRepository <Auto>
+{
+private:
+
+	vector <Auto> AutoInMemoryRepo;
+
+public:
+	AutoInRepo();
+
+	~AutoInRepo();
+
+	Auto findOne(int id);
+
+	Auto findAll();
+
+	Auto save();
+
+	Auto del();
+
+	Auto update();
+
+};
